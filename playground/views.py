8311158b68c248fe2e9daab789788109db35fe21
시아아빠 <ssep4u@gmail.com>
2023-03-22 10:11:42 +0900

    urls, views, templates playground/hello_html/ -> say_hello_html() -> playground/hello.html

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def say_hello(request):
     return HttpResponse("Hello, World!")
+
+
+def say_hello_html(request):
+    return render(request, "playground/hello.html")
