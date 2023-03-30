ddce32714eb3ba0463559ba4c2d8840fd3107b37
시아아빠 <ssep4u@gmail.com>
2023-03-30 15:12:20 +0900

    templates/아이브/멤버.html

--- 아이브/views.py
+++ 아이브/views.py
@@ -6,7 +6,7 @@ def show_원영(request):
         'name': '장원영',
         'img_src': 'https://images.khan.co.kr/article/2023/02/09/news-p.v1.20230209.b6b89ecfb36e41dc945fd92e9114751b_P1.jpg',
     }
-    return render(request, '아이브/원영.html', context=context)
+    return render(request, '아이브/멤버.html', context=context)
 
 
 def show_유진(request):
@@ -14,4 +14,4 @@ def show_유진(request):
         'name': '안유진',
         'img_src': 'https://img.sbs.co.kr/newsnet/etv/upload/2022/08/01/30000780359_1280.jpg',
     }
-    return render(request, '아이브/유진.html', context=context)
+    return render(request, '아이브/멤버.html', context=context)
